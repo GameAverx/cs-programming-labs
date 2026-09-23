ec488fce3be55d7617d0bb675a14b44ef6bbd966
Alex <trueeehouse@gmail.com>
2026-09-23 12:36:55 +1000

    feat: start lab 1

---
+++ lab1/1.py
@@ -0,0 +1,26 @@
+# 1
+
+
+station_name = "Ocean"
+active_sensors = 18
+average_temperature = 21.6
+is_online = True
+
+# Ocean <class 'str'>
+# 18 <class 'int'>
+# 21.6 <class 'float'>
+# True <class 'bool'>
+
+
+print(f"{station_name} {type(station_name)}")
+print(f"{active_sensors} {type(active_sensors)}")
+print(f"{average_temperature} {type(average_temperature)}")
+print(f"{is_online} {type(is_online)}")
+
+
+
+
+
+
+
+
---
+++ lab1/2.py
@@ -0,0 +1,11 @@
+
+a = 7
+b = 4
+c = 3
+
+ans = (a + b) * c**2 - a // b + a % b
+
+print(ans)
+
+
+
